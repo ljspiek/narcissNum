cc6e8ef3a93eadd453dddc219774f691e66b18eb
Laura Spieker <ljspiek@gmail.com>
2020-07-28 21:06:10 -0500

    solved with nested if statements

--- narcissistic.js
+++ narcissistic.js
@@ -1,5 +1,17 @@
 function narcissistic(value) {
   if (value < 10) {
     return true;
+  } else {
+    let string = value.toString();
+    let arr = string.split("");
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+      total += Math.pow(arr[i], string.length);
+    }
+    if (total === value) {
+      return true;
+    } else return false;
   }
 }
+
+console.log(narcissistic(371));
